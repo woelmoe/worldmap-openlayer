4d85db1d975674534104b6d7c3f822be4b6d78da
w <nikskilfly@gmail.com>
2023-06-25 18:21:32 +0300

    Добавлены стили для popup

--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="map" class="map">
-    <div id="popup">234234</div>
+    <div id="popup" class="ol-popup">234234</div>
+    <a href="#" id="popup-closer" class="ol-popup-closer"></a>
   </div>
 </template>
 
@@ -110,4 +111,47 @@ onMounted(() => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.ol-popup {
+  position: absolute;
+  background-color: white;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+  padding: 15px;
+  border-radius: 10px;
+  border: 1px solid #cccccc;
+  bottom: 12px;
+  left: -50px;
+  min-width: 280px;
+}
+.ol-popup:after,
+.ol-popup:before {
+  top: 100%;
+  border: solid transparent;
+  content: ' ';
+  height: 0;
+  width: 0;
+  position: absolute;
+  pointer-events: none;
+}
+.ol-popup:after {
+  border-top-color: white;
+  border-width: 10px;
+  left: 48px;
+  margin-left: -10px;
+}
+.ol-popup:before {
+  border-top-color: #cccccc;
+  border-width: 11px;
+  left: 48px;
+  margin-left: -11px;
+}
+.ol-popup-closer {
+  text-decoration: none;
+  position: absolute;
+  top: 2px;
+  right: 8px;
+}
+.ol-popup-closer:after {
+  content: '✖';
+}
+</style>
